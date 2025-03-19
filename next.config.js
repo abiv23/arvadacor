9286780a1b87d621c1ecf6900a8ce034048faf97
abiv23 <abiv23@gmail.com>
2025-03-18 18:29:27 -0600

    updated config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  distDir: process.env.NODE_ENV === "production" ? "build" : ".next",
-  experimental: {
-    turbo: false
-  }
+  // Disable experimental features entirely
+  experimental: {}
 };
 
 module.exports = nextConfig;
